eb13bbede65e7f6e3ea0ac0664f43504a465b168
dailywoes <johnrteixeira@gmail.com>
2021-12-22 13:24:51 -0500

    Initialized Project

---
+++ src/test/java/com/johnrteixeira/spring5jokesapp/Spring5jokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnrteixeira.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5jokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
